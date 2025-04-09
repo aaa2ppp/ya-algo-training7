9ee202c7283403f06065e9b035663db0876ca6ae
Aleksandr Panfilov <aaa2ppp@gmail.com>
2025-04-09 23:05:06 +0300

    less1/a done

---
+++ less1/a+/main_test.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+)
+
+func Test_run(t *testing.T) {
+	type args struct {
+		in io.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantOut string
+		debug   bool
+	}{
+		{
+			"1",
+			args{strings.NewReader(`1 1
+1
+2
+`)},
+			`1
+1 
+`,
+			true,
+		},
+		{
+			"2",
+			args{strings.NewReader(`1 1
+1
+1
+`)},
+			`0
+0 
+`,
+			true,
+		},
+		{
+			"0.3",
+			args{strings.NewReader(`2 2
+3 2
+4 3`)},
+			`2
+1 2`,
+			true,
+		},
+		{
+			"0.4",
+			args{strings.NewReader(`3 3
+5 4 2
+5 5 5`)},
+			`2
+0 2 1`,
+			true,
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer func(v bool) { debugEnable = v }(debugEnable)
+			debugEnable = tt.debug
+			out := &bytes.Buffer{}
+			run(tt.args.in, out)
+			if gotOut := out.String(); trimLines(gotOut) != trimLines(tt.wantOut) {
+				t.Errorf("run() = %v, want %v", gotOut, tt.wantOut)
+			}
+		})
+	}
+}
+
+func trimLines(text string) string {
+	lines := strings.Split(text, "\n")
+	for i, line := range lines {
+		lines[i] = strings.TrimRight(line, " \t\r\n")
+	}
+	for n := len(lines); n > 0 && lines[n-1] == ""; n-- {
+		lines = lines[:n-1]
+	}
+	return strings.Join(lines, "\n")
+}
