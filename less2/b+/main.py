a6bd533e50e8f20d9ba6efa5eb7b397249af0f12
Aleksandr Panfilov <aaa2ppp@gmail.com>
2025-04-17 22:16:09 +0300

    less2/b python solution less than 1s

--- less2/b+/main.py
+++ less2/b+/main.py
@@ -1,49 +1,31 @@
 def precalc(nums):
-    n = len(nums)
     tab = []
-
-    pow = [0]*(n + 1)
-    j = 1
-
-    row = [(i, nums[i]) for i in range(n)]
+    row = tuple((v << 32) + i for i, v in enumerate(nums, 1))  # 1-base
     tab.append(row)
-
     prev, k, s = row, 0, 1
+    pow = [0, 0, 0]
+
     while s < len(prev):
-        while j <= s*2 and j < len(pow):
-            pow[j] = k
-            j += 1
-        row = [prev[i] if prev[i][1] > prev[i+s][1] else prev[i+s]
-               for i in range(len(prev)-s)]
+        row = tuple(prev[i] if prev[i] > prev[i+s] else prev[i+s] for i in range(len(prev)-s))
         tab.append(row)
         prev, k, s = row, k+1, s*2
-
-    while j < len(pow):
-        pow[j] = k
-        j += 1
+        pow.extend([k]*(s*2+1-len(pow)))
 
     return tab, pow
 
 
-def query(tab, pow, l, r):
-    s = r - l + 1
-    k = pow[s]
-    a, b = tab[k][l], tab[k][r+1-(1 << k)]
-    return a[0] if a[1] > b[1] else b[0]
-
-
 def main():
-    n = int(input())
-    nums = tuple(map(int, input().split()))
+    _ = int(input())
+    nums = map(int, input().split())
     tab, pow = precalc(nums)
     k = int(input())
     ans = []
     for _ in range(k):
         l, r = map(int, input().split())
-        l, r = l-1, r-1
-        idx = query(tab, pow, l, r)
-        ans.append(idx+1)
-    print(*ans, sep="\n")
+        k = pow[r - l + 1]
+        a, b = tab[k][l-1], tab[k][r-(1 << k)]
+        ans.append(a & 0xffff_ffff if a > b else b & 0xffff_ffff)
+    print("\n".join(map(str, ans)))
 
 
 main()
